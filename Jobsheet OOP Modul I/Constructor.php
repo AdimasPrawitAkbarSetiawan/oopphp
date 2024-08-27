0b31337ed74c457f8ac906702b58e7c050ef3746
AdimasPrawitAkbarSetiawan <dmas4434@gmail.com>
2024-08-27 07:59:30 +0700

    update

---
+++ Jobsheet OOP Modul I/Constructor.php
@@ -0,0 +1,30 @@
+<?php
+class Mahasiswa
+{
+    // Atribut
+    public $nama;
+    public $nim;
+    public $jurusan;
+
+    // Constructor untuk menginisialisasi atribut
+    public function __construct($nama, $nim, $jurusan)
+    {
+        $this->nama = $nama;
+        $this->nim = $nim;
+        $this->jurusan = $jurusan;
+    }
+
+    // Metode untuk menampilkan data mahasiswa
+    public function tampilkanData()
+    {
+        echo "Nama: " . $this->nama . "<br>";
+        echo "NIM: " . $this->nim . "<br>";
+        echo "Jurusan: " . $this->jurusan . "<br>";
+    }
+}
+
+// Instansiasi objek dari class Mahasiswa dengan constructor
+$mahasiswa2 = new Mahasiswa("Ani", "230202002", "Sistem Informasi");
+
+// Tampilkan data mahasiswa
+$mahasiswa2->tampilkanData();
